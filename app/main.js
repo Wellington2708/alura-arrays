bb0a203cf3b9e21fa3dea04da930b2288c2508b9
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-01-07 20:47:27 +0000

    trabalhando com map

--- app/main.js
+++ app/main.js
@@ -7,8 +7,8 @@ const elementoParaInserirLivros = document.getElementById("livros");
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endPointDaApi);
     livros = await res.json();
-    console.table(livros);
-    exibirOsLivrosNaTela(livros);
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirOsLivrosNaTela(livrosComDesconto);
 }
 
 function exibirOsLivrosNaTela(listaDeLivros){
@@ -20,7 +20,7 @@ function exibirOsLivrosNaTela(listaDeLivros){
           ${livro.titulo}
         </h2>
         <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
         <div class="tags">
           <span class="tag">${livro.categoria}</span>
         </div>
